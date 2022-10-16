5bc01c42954af68bd19ecf4e9b25db0534438578
managerboard <syplus@sina.com>
2022-10-16 15:14:04 +0800

    code

--- code/a.sh
+++ code/a.sh
@@ -2,14 +2,14 @@
 
 cd $1/pic
 i=1
-while [ $i -le 2 ]
+while [ $i -le 5 ]
 do
 x=`date`.png
 screencapture -xra "$x"
 git add  "$x"
 git commit -m "$x"
 let i++
-sleep 5
+sleep 60
 done
 git push
 
