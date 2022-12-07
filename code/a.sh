e31f50bf579030794eccec8a14267d664aba18ed
doris <doris@dorisdeMacBook-Air.local>
2022-12-07 10:40:01 +0800

    Wed Dec  7 10:40:00 CST 2022.png

--- code/a.sh
+++ code/a.sh
@@ -1,6 +1,13 @@
 #/bin/bash
-x=`ps -Ael |grep a.sh |wc -l`
-if [ $x -gt 1 ] ;then exit; fi
+ps -Ael|grep a.sh >/tmp/tmp
+x=`cat /tmp/tmp |grep a.sh |wc -l`
+#x=`ps -Ael |grep a.sh |wc -l`
+#debug start...
+echo "==>",$x >>/tmp/a
+cat /tmp/tmp >>/tmp/a
+ps -Ael|grep a.sh >>/tmp/a
+#debug end...
+if [ $x -gt 2 ] ;then exit; fi
 
 cd $1/pic
 i=1
